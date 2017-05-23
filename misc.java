f59e96591c8e8abc66fbb8d8488ec8f19d7e61db
David Kheri <david.kheri@yahoo.com>
2017-05-23 17:59:40 +0200

    misc was readded
    For real this time

---
+++ misc.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.*;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Scanner;
+
+public class misc
+{
+   public static void main(String[] args) throws IOException
+      {
+      int x=1;
+      int y=2;      
+      System.out.println(x+"\n"+y);      
+      }
+}
